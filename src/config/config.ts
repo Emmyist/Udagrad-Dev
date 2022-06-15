a4a8457aa9ebc1f8740b0684667911e2e85c1ff9
Emmyist <adesiteadewole@gmail.com>
2022-06-15 12:29:21 +0000

    initital commit

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "postgres",
+    "password": "babsAdesite7",
+    "database": "emmyistUdagramDev",
+    "host": "database-1.cnza49spkoko.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "emmyist-udagram-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
